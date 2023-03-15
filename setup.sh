6a051a3dfa4d57bea61bfb8316eb7542e26d13d8
Mike Groenewald <michael.groenewald@gmail.com>
2023-03-15 18:05:57 +0000

    Making it humble compatable

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+#vcs import < src/ros2.repos src
+sudo apt-get update
+
+sudo apt install -y python3-natsort  # Installing natsort (is not in rosdep)
+
+rosdep update
+rosdep install --from-paths src --ignore-src -y
